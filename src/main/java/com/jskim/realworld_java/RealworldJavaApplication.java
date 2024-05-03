5083871607cb65331c94fe8b78726fb736724fc3
js.kim <js.kim@dfocus.net>
2024-05-03 11:08:30 +0900

    Initial commit

---
+++ src/main/java/com/jskim/realworld_java/RealworldJavaApplication.java
@@ -0,0 +1,13 @@
+package com.jskim.realworld_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealworldJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jskim/realworld_java/RealworldJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jskim.realworld_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
